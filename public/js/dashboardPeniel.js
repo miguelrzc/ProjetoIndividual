89182a0de0d607803dbe7a42960fa1b43c520a9b
miguelrzc <miguel.carvalho@sptech.school>
2026-05-08 23:57:12 -0300

    style
    alterando o tipo de gráfico, e estilizando

--- public/js/dashboardPeniel.js
+++ public/js/dashboardPeniel.js
@@ -1,5 +1,5 @@
-const dadosDevocionais = [1, 0, 1, 1, 2, 0, 1]; 
-const temas = { labels: ['Sabedoria', 'Fé', 'Amor'], valores: [10, 5, 3] };
+const dadosDevocionais = [0, 1, 2, 3, 4, 5, 6]; 
+const temas = { labels: ['Sabedoria', 'Fé', 'Amor','Gratidão', 'Promessas'], valores: [10, 5, 3, 3, 7] };
 
 const ctxLinha = document.getElementById('graficoLinha').getContext('2d');
 new Chart(ctxLinha, {
@@ -10,7 +10,7 @@ new Chart(ctxLinha, {
             label: 'Devocionais Feitos',
             data: dadosDevocionais,
             borderColor: '#16a085',
-            backgroundColor: 'rgba(22, 160, 133, 0.2)',
+            backgroundColor: 'rgba(22, 160, 132, 0.77)',
             fill: true,
             tension: 0.3 
         }]
@@ -21,14 +21,27 @@ new Chart(ctxLinha, {
     }
 });
 
-const ctxPizza = document.getElementById('graficoPizza').getContext('2d');
-new Chart(ctxPizza, {
-    type: 'doughnut',
+const ctxBarras = document.getElementById('graficoPizza').getContext('2d'); 
+
+new Chart(ctxBarras, {
+    type: 'bar',
     data: {
         labels: temas.labels,
         datasets: [{
-            data: temas.valores,
-            backgroundColor: ['#2c3e50', '#16a085', '#bdc3c7']
+            label: 'Total de Estudos',
+            data: [3, 2, 5, 6, 2],
+            backgroundColor: '#16a085', 
+            borderRadius: 5 
         }]
+    },
+    options: {
+        indexAxis: 'x', 
+        responsive: true,
+        plugins: {
+            legend: { display: false } 
+        },
+        scales: {
+            x: { beginAtZero: true }
+        }
     }
 });
